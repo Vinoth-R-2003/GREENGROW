24a69a62808d8dd9a73296d830db6b6ef867163e
Vinoth R <vinotamil2003@gmail.com>
2026-04-26 21:39:36 +0530

    LOGO

---
+++ FP/theme/static/js/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'greengrow-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/static/css/dist/styles.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch events (Network First, then Cache)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
