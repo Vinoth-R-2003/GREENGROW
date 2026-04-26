27acc3da577f304b775fe6a3b143813ad55d8963
Vinoth R <vinotamil2003@gmail.com>
2026-04-26 21:57:58 +0530

    admin

--- FP/feed/admin.py
+++ FP/feed/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content_snippet', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('content', 'author__username')
+
+    def content_snippet(self, obj):
+        return obj.content[:50]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at')
