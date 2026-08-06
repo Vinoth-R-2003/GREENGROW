b917ea617ad0ceecccdcd74f4d475aa4f30d35ef
Vinoth R <vinotamil2003@gmail.com>
2026-08-06 09:44:40 +0530

    render fail

--- FP/build.sh
+++ FP/build.sh
@@ -7,13 +7,6 @@ set -e  # Exit immediately if any command fails
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Running database migrations..."
-python manage.py migrate
-
-echo "Seeding Marketplace and Garden data..."
-python seed_items.py
-python seed_plants.py
-
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
